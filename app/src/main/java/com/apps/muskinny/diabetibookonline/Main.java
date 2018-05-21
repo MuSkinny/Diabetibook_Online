3dcefc487dfd154de8c695e7fdd332954e8306ed
Marco Falanga <marco.fala97@gmail.com>
2018-05-21 08:33:37 +0200

    added OnCLick

--- app/src/main/java/com/apps/muskinny/diabetibookonline/Main.java
+++ app/src/main/java/com/apps/muskinny/diabetibookonline/Main.java
@@ -2,12 +2,20 @@ package com.apps.muskinny.diabetibookonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
     }
 }
